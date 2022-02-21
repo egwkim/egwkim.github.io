420776323c01d5eb38b84c63b8f0f387f82d4455
egwkim <35051914+egwkim@users.noreply.github.com>
2022-02-21 11:37:11 +0900

    Feature dark theme

---
+++ dark-theme.js
@@ -0,0 +1,34 @@
+let theme = 'light';
+
+function setTheme(color) {
+  theme = color;
+  if (theme == 'dark') {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+  localStorage.setItem('theme', theme);
+}
+
+function toggleTheme() {
+  if (theme == 'light') {
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+}
+
+function checkTheme() {
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+  const currentTheme = localStorage.getItem('theme');
+  if (
+    (currentTheme != 'light' && prefersDarkScheme.matches) ||
+    currentTheme == 'dark'
+  ) {
+    setTheme('dark');
+    document.getElementById('theme-checkbox').checked = true;
+  } else {
+    setTheme('light');
+    document.getElementById('theme-checkbox').checked = false;
+  }
+}
